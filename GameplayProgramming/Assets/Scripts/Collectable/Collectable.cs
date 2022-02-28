636ecc8c0de1643c19193873464c6db245465199
Kye Dudley <Kye2.Dudley@live.uwe.ac.uk>
2022-02-28 16:55:57 +0000

    Collectable Script

---
+++ GameplayProgramming/Assets/Scripts/Collectable/Collectable.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    enum collectableTypes
+    {
+        Coin,
+        DoubleJump,
+        SpeedBoost
+    };
+    collectableTypes Type;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            switch(Type)
+            {
+                case collectableTypes.Coin:
+                    Debug.Log("Increase Score");
+                    break;
+                case collectableTypes.DoubleJump:
+                    Debug.Log("Enable Double Jump");
+                    break;
+                case collectableTypes.SpeedBoost:
+                    Debug.Log("Enable Speed Boost");
+                    break;
+            }
+            Destroy(this.gameObject);
+        }
+    }
+    private void FixedUpdate()
+    {
+        transform.Rotate(0, 3, 0);
+    }
+}
