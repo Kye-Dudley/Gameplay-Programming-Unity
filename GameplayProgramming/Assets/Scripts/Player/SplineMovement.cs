c22879e96f8ae2dd899ca03d190b12dccb0a79b4
Kye Dudley <Kye2.Dudley@live.uwe.ac.uk>
2022-03-29 10:19:49 +0100

    Spline Movement Controller now updates in LocalPosition

--- GameplayProgramming/Assets/Scripts/Player/SplineMovement.cs
+++ GameplayProgramming/Assets/Scripts/Player/SplineMovement.cs
@@ -7,19 +7,21 @@ public class SplineMovement : MonoBehaviour
 {
     private Vector2 movementVector;
     PlayerInput controller;
+    FollowSpline followSplineScript;
     Vector3 currentOffsetPos;
+
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<PlayerInput>();
+        followSplineScript = GetComponentInParent<FollowSpline>();
+        
         controller.enabled = false;
     }
     private void OnMove(InputValue movementValue)
     {
         movementVector = movementValue.Get<Vector2>();
-        Debug.Log("Spline Controller Input Recieved!");
-//        Vector3.Lerp((transform.position), new Vector3(movementVector.x, movementVector.y, transform.position.z), Time.deltaTime * 2);
-        transform.position = new Vector3(transform.position.x, movementVector.y + transform.position.y, movementVector.x + transform.position.z);
+        transform.localPosition = new Vector3(movementVector.x * 2, movementVector.y * 2, 0);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -28,6 +30,7 @@ public class SplineMovement : MonoBehaviour
         {
             other.GetComponent<PlayerInput>().enabled = false;
             controller.enabled = true;
+            followSplineScript.canMove = true;
         }
     }
 
